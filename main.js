37b8d1808ac8bb8be8afd826722af2d9ad837c6c
Sezimar Jr <sezimar_jrcs@hotmail.com>
2023-09-22 18:05:42 -0300

    Initial commit

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
-const form = document.getElementById("validate-form");
+$(document).ready(function () {
+  $("form").on("submit", function (ev) {
+    ev.preventDefault();
+    const inputValue = $("#task-input").val();
 
-const messagePrint = document.querySelector(".message");
-
-form.addEventListener("submit", function (ev) {
-  ev.preventDefault();
-
-  let valorA = parseInt(document.getElementById("valor-a").value);
-  let valorB = parseInt(document.getElementById("valor-b").value);
-  messagePrint.style.backgroundColor = "";
-  const sucessMessage = `O Valor de B (${valorB}) é maior que o valor A (${valorA})`;
-
-  if (valorB > valorA) {
-    messagePrint.innerHTML = sucessMessage;
-    messagePrint.style.display = "block";
-  } else {
-    messagePrint.innerHTML = `O Valor de B (${valorB}) não é maior que o valor A (${valorA})`;
-    messagePrint.style.backgroundColor = "red";
-    messagePrint.style.display = "block";
-  }
-});
-
-form.addEventListener("reset", function (ev) {
-  messagePrint.style.display = "none";
+    if (inputValue) {
+      const novoItem = $("<li style='display:none'></li>");
+      $(novoItem).html(inputValue);
+      $(novoItem).appendTo("ul");
+      $(novoItem).fadeIn();
+      $("#task-input").val("");
+      $(novoItem).click(function () {
+        $(this).toggleClass("done");
+      });
+    }
+  });
 });
