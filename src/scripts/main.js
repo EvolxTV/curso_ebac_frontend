959c3e32eff9354f0399abb14d543039e520fb64
Sezimar Jr <sezimar_jrcs@hotmail.com>
2023-10-21 20:12:37 -0300

    initial commit

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("form-hello")
-    .addEventListener("submit", function (ev) {
-      ev.preventDefault();
-      alert("HELLO :D");
-    });
-});
