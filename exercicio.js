e4135c781a0e16cd2cf5cfbd783fd410d67655bc
Sezimar Jr <sezimar_jrcs@hotmail.com>
2024-08-15 11:13:01 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+const multResult = multiplicacao(20, 10);
+console.log(multResult);
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+const oiSezimar = dizOla("Sezimar");
+console.log(oiSezimar);
---
+++ exercicio.ts
@@ -0,0 +1,15 @@
+function multiplicacao(a: number, b: number): number {
+  return a * b;
+}
+
+const multResult = multiplicacao(20, 10);
+
+console.log(multResult);
+
+function dizOla(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+const oiSezimar = dizOla("Sezimar");
+
+console.log(oiSezimar);
